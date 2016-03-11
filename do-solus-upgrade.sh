766f7c19f776a508cf08ce20ddb002a453463f47
Ikey Doherty <ikey@solus-project.com>
2016-03-11 02:32:58 +0000

    Add initial dummy skeleton
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ do-solus-upgrade.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# This file is part of solus-upgrade
+# 
+# Copyright (C) 2015 Ikey Doherty <ikey@solus-project.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+exit 0
