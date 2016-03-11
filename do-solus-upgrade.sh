b9ca923f18c2c54a967394879db35bf56097b047
Ikey Doherty <ikey@solus-project.com>
2016-03-11 02:59:03 +0000

    Add the rest of the steps in
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- do-solus-upgrade.sh
+++ do-solus-upgrade.sh
@@ -10,9 +10,19 @@
 # (at your option) any later version.
 #
 
+oddPackages=(python-reportlab python-lxml)
+badProcesses=(evolve-sc solus-sc pisi eopkg)
+
 function do_die()
 {
     echo "Fatal: $*"
+    exit 1
+}
+
+function do_eopkg_error()
+{
+    echo "eopkg upgrade did not complete successfully. Please re-run this script"
+    echo "If this issue persists, please seek support and do _not_ reboot"
     exit 1
 }
 
@@ -32,7 +42,7 @@ function removePackage()
 {
     local pkg="$*"
 
-    eopkg rm --ignore-safety --ignore-comar "$pkg"
+    eopkg rm --ignore-safety --ignore-comar -y "$pkg"
 }
 
 function addRepo()
@@ -41,6 +51,22 @@ function addRepo()
     local url="$2"
 
     eopkg add-repo "$repo" "$url" || do_die "Failed to add repo: $repo"
+}
+
+# *REALLY* kill it.
+function killComar()
+{
+    killall -9 comar 2>/dev/null; sleep 1; killall -9 comar 2>/dev/null;
+}
+
+# Ensure the given process is *not* running
+function checkDead()
+{
+    local badP="$*"
+
+    if [[ `/bin/ps aux | /bin/grep -v grep | /bin/grep "${badP}"` ]]; then
+        do_die "Cannot continue as ${badP} is still running. Please ensure it is closed."
+    fi
 }
 
 # Remove all listed repos
@@ -53,9 +79,27 @@ fi
 # Add the Shannon Solus repo
 addRepo Solus "https://packages.solus-project.com/shannon/eopkg-index.xml.xz"
 
-oddPackages=(python-reportlab python-lxml)
-
 for pkg in "${oddPackages[@]}"; do
     # This is fine to fail.
     removePackage "$pkg"
 done
+
+for badPs in "${badProcesses[@]}"; do
+    checkDead "${badPs}"
+done
+
+# Must be dead due to the Python UCS4 migration
+killComar
+checkDead comar
+
+# Now we continue with the upgrade, it's up to the user to hit yes.
+eopkg upgrade --ignore-comar --ignore-file-conflicts || do_eopkg_error
+
+# Ensure comar really dead.
+killComar
+
+# No, seriously.
+checkDead comar
+
+# Now configure any pending, bobs your uncle, etc.
+eopkg configure-pending
